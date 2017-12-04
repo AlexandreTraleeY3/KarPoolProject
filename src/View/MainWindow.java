9f4603bebe0a3b999855e55383a6ead53882e087
alexandre <careyalexandre@gmail.com>
2017-12-04 12:48:02 +0000

    Main APplication created

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -6,17 +6,23 @@ import java.awt.*;
 public class MainWindow {
     //1. Create the frame.
     JFrame frame = new JFrame("FrameDemo");
+    Label emptyLabel;
 
-//2. Optional: What happens when the frame closes?
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    public MainWindow()
+    {
+        //2. Optional: What happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        emptyLabel = new Label();
+        emptyLabel.setText("Hello World!");
+        System.out.println(emptyLabel.getText());
 //3. Create components and put them in the frame.
 //...create emptyLabel...
-    frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
 //4. Size the frame.
-    frame.pack();
+        frame.pack();
 
 //5. Show it.
-    frame.setVisible(true);
+        frame.setVisible(true);
+    }
 }
