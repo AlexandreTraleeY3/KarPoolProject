49353655f46b4151303b83c03659de3135e0d33e
t00198379 <careyalexandre@gmail.com>
2017-12-05 09:59:16 +0000

    Mainwindow implementation

---
+++ src/View/TripWindow.java
@@ -0,0 +1,15 @@
+package View;
+
+import javax.swing.*;
+
+public class TripWindow extends JFrame {
+    private JScrollBar scrollBar1;
+    private JTabbedPane tabbedPane1;
+
+
+    public TripWindow() {
+
+        this.setVisible(true);
+    }
+
+}
